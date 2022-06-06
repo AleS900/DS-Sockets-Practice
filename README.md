145a9a5b92692a1598f758b3446b98f01f3db9c3
AleS900 <santiavctf@gmail.com>
2022-06-06 14:08:53 -0400

    Sockets Practice Complete

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # DS-Sockets-Practice
+
+## Pasos para la Ejecución
+1. Abra un socket.
+2. Abra un flujo de entrada y un flujo de salida al socket.
+3. Lea y escriba en la transmisión de acuerdo con el protocolo del servidor.
+4. Cierre los flujos.
+5. Cierre el socket.
---
+++ src/main/java/Server.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private final ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer() {
+        try {
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                System.out.println("¡Un nuevo cliente se ha conectado!");
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread thread = new Thread(clientHandler);
+                // The start method begins the execution of a thread.
+                // When you call start() the run method is called.
+                // The operating system schedules the threads.
+                thread.start();
+            }
+        } catch (IOException e) {
+            closeServerSocket();
+        }
+    }
+
+    public void closeServerSocket() {
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1234);
+        Server server = new Server(serverSocket);
+        server.startServer();
+    }
+
+}
